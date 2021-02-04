262b2e8beb82cfc247730911ff4d70909613d38d
Yashwanth Reddy <yash868686reddy@gmail.com>
2021-02-03 19:10:46 -0500

    Done with slides

--- Javascript/Slider/Example1-Slider/src/index.js
+++ Javascript/Slider/Example1-Slider/src/index.js
@@ -1,2 +1,56 @@
-console.log('yashwanth');
-console.log('reddy');
+function Slider(slider) {
+  if (!(slider instanceof Element)) {
+    throw new Error('No slider passed in');
+  }
+
+  let current;
+  let prev;
+  let next;
+
+  const slides = slider.querySelector('.slides');
+  const prevButton = slider.querySelector('.goToPrev');
+  const nextButton = slider.querySelector('.goToNext');
+
+  function startSlider() {
+    current = slider.querySelector('.current') || slides.firstElementChild;
+    prev = current.previousElementSibling || slides.lastElementChild;
+    next = current.nextElementSibling || slides.firstElementChild;
+    console.log({ current, prev, next });
+  }
+
+  function applyClasses() {
+    current.classList.add('current');
+    prev.classList.add('prev');
+    next.classList.add('next');
+  }
+  function moveDirection(direction) {
+    const classesToRemove = ['prev', 'current', 'next'];
+    prev.classList.remove(...classesToRemove);
+    current.classList.remove(...classesToRemove);
+    next.classList.remove(...classesToRemove);
+    if (direction === 'back') {
+      [prev, current, next] = [
+        prev.previousElementSibling || slides.lastElementChild,
+        prev,
+        current,
+      ];
+    } else {
+      [prev, current, next] = [
+        current,
+        next,
+        next.nextElementSibling || slides.firstElementChild,
+      ];
+    }
+    applyClasses();
+  }
+  startSlider();
+  applyClasses();
+
+  // Event Listeners
+
+  prevButton.addEventListener('click', () => moveDirection('back'));
+  nextButton.addEventListener('click', moveDirection);
+}
+
+const mySlider = Slider(document.querySelector('.slider'));
+const dogSlider = Slider(document.querySelector('.dog-slider'));
